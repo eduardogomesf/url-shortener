5921f1380ecd39e63da4a677d1e489c1a6bca27c
eduardogomesf <eduardogoomesf@gmail.com>
2025-11-16 13:15:41 -0300

    feat: add swagger, global pipelines with configuration usage in starter file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Configuration } from '@/shared/config';
+import { version } from 'package.json';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configs = app.select(AppModule).get(Configuration);
+
+  app.setGlobalPrefix(configs.app.basePath);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfigs = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription(
+      'This service is responsible for URL shortening and its management',
+    )
+    .setVersion(version)
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfigs);
+
+  SwaggerModule.setup(`${configs.app.basePath}`, app, swaggerDoc, {
+    jsonDocumentUrl: `${configs.app.basePath}/json`,
+  });
+
+  await app.listen(configs.app.port, () =>
+    logger.log(`Running on ${configs.app.port}`),
+  );
 }
 bootstrap();
